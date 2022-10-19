0e18a0d0a1014c63e0e588cc67e5e91d4bd396c2
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-10-19 21:32:53 +0300

    components

--- kolafit/src/App.js
+++ kolafit/src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
-import Signup from "./Signup";
+import Signup from "./components/SignUp";
+
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       
-        <Signup/>
-        
+<Signup/>        
     </div>
   );
 }
