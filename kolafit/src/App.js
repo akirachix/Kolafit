f8910abac1a3e4638ecda1f6383b698e92ec3b5b
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-10-20 21:38:34 +0300

    Button routing

--- kolafit/src/App.js
+++ kolafit/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Signup from "./components/SignUp";
 import Login from "./components/Login";
+import LandingPage from "./components/Home/Landingpage";
 
 
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
     <Router>
       <Routes>
+        <Route path="/home" element={ <LandingPage/> }/>
        <Route path="/signup" element={<Signup/>} component= {<Signup/>}/>
         <Route path="/login" element={<Login/>} component= {<Login/>}/>
       </Routes>
