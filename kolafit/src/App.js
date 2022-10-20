a11932b37efd86b009eebfb014aa79474d672139
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-10-20 19:16:04 +0300

    routing

--- kolafit/src/App.js
+++ kolafit/src/App.js
@@ -1,14 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Signup from "./components/SignUp";
+import Login from "./components/Login";
+
 
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
-<Signup/>        
-    </div>
+    <Router>
+      <Routes>
+       <Route path="/signup" element={<Signup/>} component= {<Signup/>}/>
+        <Route path="/login" element={<Login/>} component= {<Login/>}/>
+      </Routes>
+    </Router>
+  </div>
+
+     
   );
 }
 
