c9cd944400f887b276cf9441d8f249dda5f22d3b
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-10-20 07:50:34 +0300

    Components

--- kolafit/src/components/SignUp/index.jsx
+++ kolafit/src/components/SignUp/index.jsx
@@ -2,55 +2,56 @@ import React from 'react';
 import "../SignUp/styles.css"
 import { useState } from 'react';
 import axios from 'axios'; 
+import kola from "../Kola.png"
+
 
 
 const Signup = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const [full_name, setFullName] = useState("");
-    const [gender, setGender] = useState("");
+
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [confirmPassword, setConfirmPassword] = useState("");
+const [fullname, setFullName] = useState("");
+const [gender, setGender] = useState("");
 
 
-    const submitting = (event) => {
-        event.preventDefault()
-        const user = {full_name, gender,password, email }
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-        const requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: JSON.stringify(
-                user
-            ),
-            redirect: 'login'
-        };
-       
-    }
-
+const submitting = (event) => {
+    event.preventDefault()
+    const user = {fullname, gender,password, email }
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: JSON.stringify(
+            user
+        ),
+        redirect: 'login'
+    };
    
-        axios   
-          .get("http://localhost:8000/api/tasks/")
-          .then(res => this.setState({ taskList: res.data }))
-          .catch(err => console.log(err));
-    
-    return (
-        <div className="body">
-           
+}
+
+
+
+  return(
+    <div className='signUp'>
+      <div className='main'>
+         <img src={kola} height = "500" width = "400"/>
+      </div>
+
+      <div className='form'>
                 <h2 className='signup'>Sign Up</h2>
                 <form onSubmit={submitting}>
                     <label>
-
                         <div className="fullName">
                             <input className="container"
                                 type="text"
                                 placeholder="Full Name"
-                                value={full_name}
+                                value={fullname}
                                 onChange={(e) => {
                                     setFullName(e.target.value)
                                 }}
                             ></input></div>
-
                         <div className="gender">
                             <input className="container"
                                 type="text"
@@ -60,7 +61,6 @@ const Signup = () => {
                                     setGender(e.target.value)
                                 }}
                             ></input></div>
-
                         <div className="email">
                             <input className="container"
                                 type="text"
@@ -69,11 +69,9 @@ const Signup = () => {
                                 onChange={(e) => {
                                     setEmail(e.target.value);
                                 }
-                                
                                 }
                             ></input></div>
                         <div className="password">
-                            <h5> </h5>
                             <input className="container"
                                 type="password"
                                 placeholder="Password"
@@ -81,7 +79,6 @@ const Signup = () => {
                                 onChange={(e) => {
                                     setPassword(e.target.value);
                                 }
-                                
                                 }
                             ></input><br></br></div>
                         <div className="confirm_password">
@@ -96,18 +93,17 @@ const Signup = () => {
                                 }
                             ></input><br></br></div>
                     </label>
-
                     <button className="button" type='submit' >SignUp</button>
                 </form>
                 <p className="account">Have an account?
                     <a href="/Login"><span >   Login   </span> </a> </p>
-
-            </div>  
-
-           
-
-       
-    )
+            </div>
+        </div>
     
-};
+  )}
+    
+    
+
+
+   
 export default Signup;
