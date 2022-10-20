c8ab0dcfb44a546a8ac50fa23115ca9741999949
mercy-ndungu <mercyndungukirigo@gmail.com>
2022-10-20 10:55:04 +0300

    Added components

---
+++ kolafit/src/Components/Account.js
@@ -0,0 +1,17 @@
+import {Link} from 'react-router-dom'
+
+const Account = ()=> {
+    return (
+        <div>
+          
+            <div>
+                <Link to = '/landingpage' className='btn-account'>
+                    Back
+                </Link>
+            </div>
+
+        </div>
+    );
+
+};
+export default Account;
---
+++ kolafit/src/Components/Education.js
@@ -0,0 +1,17 @@
+import {Link} from 'react-router-dom'
+
+const Education = ()=> {
+    return (
+        <div>
+          
+            <div>
+                <Link to = '/landingpage' className='btn-education'>
+                    Back
+                </Link>
+            </div>
+
+        </div>
+    );
+
+};
+export default Education;
