b0769a60ddb148d7ebefb2c9937a9427c8edf0a6
Wangariiisharon <mwangiiisharon@gmail.com>
2022-10-21 09:06:42 +0300

    Upload Details, about and Splashscren

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Signup from "./components/SignUp"; 
+import Splashscreen from "./components/splashscreen"
+import Details from "./components/UploadDetails" 
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      
+    {/* <Splashscreen/>      */}
+    <Details/>     
+    </div> 
+    
+  );
+}
+
+export default App;
