34960495bbeeb8bf7b0027f73565d54ff73a84c3
mercy-ndungu <mercyndungukirigo@gmail.com>
2022-10-25 18:15:21 +0300

    Modified landing page

--- src/components/Landingpage/index.jsx
+++ src/components/Landingpage/index.jsx
@@ -1,15 +1,10 @@
-// import React from 'react';
 import {Link} from 'react-router-dom';
-import myImage from '../Home.png'
-
+// import myImage from '../Home.png'
 const Landingpage = () => {
-
     return (
-        
         <div className='full-div'>
-
             <div className='landing-page-image'>
-            <img className='img' src={myImage} alt="home" style={{width: "550px", height: "250px", marginTop:"20px", marginRight:"600px",  }} />
+            {/* <img className='img' src={myImage} alt="home" style={{width: "550px", height: "250px", marginTop:"20px", marginRight:"600px",  }} /> */}
             </div>
 <div className='text'>
 <div className='first-heading'>
@@ -20,14 +15,11 @@ and makes the unified data available to financial  instutitons via an API.</h3>
 <div className='second-section'>
     <h2>Why Kolafit</h2>
     <h3>A key limitation on the eligibility of many assets as collateral is information asymmetry. Owing to the inability of <br/>
-many lenders to ascertain the true ownership of assets, and reluctant to accept many assets that could serve as <br/> 
+many lenders to ascertain the true ownership of assets, and reluctant to accept many assets that could serve as <br/>
 collateral. Thus this informational constraint renders many types of assets ineligible as collateral and limits access to credit.</h3>
 </div>
-
-
 </div>
 <div>
-    
         <Link className='sign-text' path to={"/signup"}>
             <button className='home-button'>Signup</button>
     </Link>
