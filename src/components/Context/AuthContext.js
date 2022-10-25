3edf0f2d72bdce502c51ca53e22f857166245d04
mercy-ndungu <mercyndungukirigo@gmail.com>
2022-10-25 16:00:03 +0300

    changes

---
+++ src/components/Context/AuthContext.js
@@ -0,0 +1,133 @@
+import { createContext, useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
+// import { Axios } from "axios";
+// import { useNavigate } from "react-router-dom";
+
+const AuthContext = createContext();
+
+export default AuthContext;
+
+export const AuthProvider = ({ children }) => {
+    const [authTokens, setAuthTokens] = useState(() =>
+        localStorage.getItem("authTokens")
+            ? JSON.parse(localStorage.getItem("authTokens"))
+            : null
+    );
+    const [user, setUser] = useState(() =>
+        localStorage.getItem("authTokens")
+            ? jwt_decode(localStorage.getItem("authTokens"))
+            : null
+    );
+    const [loading, setLoading] = useState(true);
+
+    // const navigate = useNavigate();
+
+
+    const registerUser = async ({ firstName,lastName, gender, email, password }) => {
+
+        const det = {
+            "first_name": firstName,
+            "lastt_name": lastName,
+            "gender": gender,
+            "email": email,
+            "password": password
+        }
+        console.log(det)
+
+        const response = await fetch(" http://127.0.0.1:8000/api/signup/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(det)
+        });
+        if (response.status === 201) {
+            // response.save(det)
+            console.log(response)
+            
+            // navigate("/login");
+        } else {
+            alert("Something went wrong!");
+        }
+    };
+
+
+
+    // const loginUser = async ({ email, password }) => {
+
+    //     const log = {
+    //         "email": email,
+    //         "password": password
+    //     }
+    //     console.log(log)
+    //     const response = await fetch("http://127.0.0.1:8000/api/login/", {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         },
+    //         body: JSON.stringify(log)
+    //     });
+    //     const data = await response.json();
+
+    //     if (response.status === 200) {
+    //         setAuthTokens(data);
+    //         setUser(jwt_decode(data.access));
+    //         localStorage.setItem("authTokens", JSON.stringify(data));
+    //         navigate("/");
+    //     } else {
+    //         alert("Something went wrong!");
+    //     }}
+        //     const response = await fetch("https://rocky-waters-56466.herokuapp.com/api/signup", {
+        //         method: "POST",
+        //         headers: {
+        //             "Content-Type": "application/json"
+        //         },
+        //         body: JSON.stringify({
+        //             email,
+        //             password
+        //         })
+        //     });
+        //     const data = await response.json();
+
+        //     if (response.status === 200) {
+        //         setAuthTokens(data);
+        //         setUser(jwt_decode(data.access));
+        //         localStorage.setItem("authTokens", JSON.stringify(data));
+        //         navigate("/");
+        //     } else {
+        //         alert("Something went wrong!");
+        //     }
+        // };
+
+    
+
+        // const logoutUser = () => {
+        //     setAuthTokens(null);
+        //     setUser(null);
+        //     localStorage.removeItem("authTokens");
+        //     navigate("/");
+        // };
+
+        const contextData = {
+            user,
+            setUser,
+            authTokens,
+            setAuthTokens,
+            registerUser,
+            // loginUser,
+            // logoutUser
+        };
+
+        useEffect(() => {
+            if (authTokens) {
+                setUser(jwt_decode(authTokens.access));
+            }
+            setLoading(false);
+        }, [authTokens, loading]);
+
+        return (
+            <AuthContext.Provider value={contextData}>
+                {loading ? null : children}
+            </AuthContext.Provider>
+        );
+        }
---
+++ src/components/Login/index.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import "./style.css";
+// import kola from "../Kola.png"
+import { Link } from "react-router-dom";
+const Login = () => {
+    const [email, setEmail] = useState("");
+    console.log(email);
+    const [password, setPassword] = useState("");
+    console.log(password);
+    const handleSubmit = () => {
+        // e.preventDefault()
+        const data = {
+            email: email,
+            password: password,
+        };
+        console.log(JSON.stringify(data));
+    };
+    return (
+        <div className="login">
+         <div className='main-container'>
+            <div className="form">
+                <h2>Login</h2>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        <div className="email">
+                            <input
+                                className="container"
+                                type="text"
+                                placeholder="Enter email"
+                                onChange={(e) => {
+                                    setEmail(e.target.value);
+                                }}
+                            ></input>
+                        </div>
+                        <div className="password">
+                            {/* <h5> </h5> */}
+                            <input
+                                className="container"
+                                type="password"
+                                placeholder="Enter password"
+                                onChange={(e) => {
+                                    setPassword(e.target.value);
+                                }}
+                            ></input>
+                            <br></br>
+                        </div>
+                        <div className="check">
+                            <input className="box" type="checkbox"></input>
+                            <div className="log">
+                            <p className="log">Keep me logged in</p>
+                            </div>
+                            
+                            <div className="forgot">
+                                <a href="/forgot"><span >   Forgot password?   </span> </a>
+                            </div>
+                        </div>
+                    </label>
+                    <Link to='/home' >
+                    <button className="button" type='submit' >Login</button>
+                    </Link>
+                </form>
+                <p className="account">Don't have an account?
+                    <span >  <Link to="/signup">Signup</Link>  </span>
+                </p>
+            </div>
+            </div>
+        </div>
+    );
+};
+export default Login;
+
+
+
+
+
