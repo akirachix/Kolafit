7605a9fa9c8fe84af20cd94a3e4cb15e4399f03f
mercy-ndungu <mercyndungukirigo@gmail.com>
2022-10-25 18:17:35 +0300

    Changed landing page

--- kolafit/src/components/Landingpage/index.jsx
+++ kolafit/src/components/Landingpage/index.jsx
@@ -1,79 +1,30 @@
-import React , {useState,useEffect} from 'react';
-import HashLoader from "react-spinners/HashLoader";
-import "../splashscreen/style.css"
-import about from "./Group 17.png"
-import logo from "./logo2.png"
-// import {Link} from 'react-router-dom'
-function Splashscreen(){
-    const [loading,setLoading]=useState(false);
-    useEffect(()=>{
-        setLoading(true)
-        setTimeout(() => {
-            setLoading(false)
-        },2000);
-    },[])
-    return(
-        <div className="splash">
-        {
-        loading ?
-        <div className='hashloader'>
-        < HashLoader   
-        color={"#4460DC"}
-        loading={loading}
-        // cssOverride={override}
-        size={130}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      />
-      </div>
-        :
-        <div className='About'>
-        <div className='Logo'>
-        <img src={logo} alt="logo"></img>
+import {Link} from 'react-router-dom';
+// import myImage from '../Home.png'
+const Landingpage = () => {
+    return (
+        <div className='full-div'>
+            <div className='landing-page-image'>
+            {/* <img className='img' src={myImage} alt="home" style={{width: "550px", height: "250px", marginTop:"20px", marginRight:"600px",  }} /> */}
+            </div>
+<div className='text'>
+<div className='first-heading'>
+<h2>About KolaFit</h2>
+<h3>KolaFit improves the creditworthiness by aggregating a customer’s data after calculating their loan eligibility <br/>
+and makes the unified data available to financial  instutitons via an API.</h3>
+</div>
+<div className='second-section'>
+    <h2>Why Kolafit</h2>
+    <h3>A key limitation on the eligibility of many assets as collateral is information asymmetry. Owing to the inability of <br/>
+many lenders to ascertain the true ownership of assets, and reluctant to accept many assets that could serve as <br/>
+collateral. Thus this informational constraint renders many types of assets ineligible as collateral and limits access to credit.</h3>
+</div>
+</div>
+<div>
+        <Link className='sign-text' path to={"/signup"}>
+            <button className='home-button'>Signup</button>
+    </Link>
+</div>
         </div>
-     
-        <a href=''>SignUp</a>
-        <img src={about} alt="Image"></img>
-    
-
-        <div className="aboutText">
-        <h2>About KolaFit</h2>
-        <p>
-        KolaFit improves the creditworthiness by aggregating
-        a customer's data after calculating their loan eligibility
-        and makes the unified data available to financial
-        instutitons via an API.
-        </p>
-
-        {/* <div className="WhyKolafit">
-        <h2>Why KolaFit</h2>
-        <p>
-        A key limitation on the eligibility of many assets as collateral
-        is information asymmetry. Owing to the inability of many
-        lenders to ascertain the true ownership of assets,<br/>
-         and reluctant
-        to accept many assets that could serve as collateral. Thus this
-        informational constraint renders many types of assets ineligible
-        as collateral and limits access to credit.
-        </p>
-        </div> */}
-        {/* <button className="button" type='submit' >SignUp</button> */}
-        </div>
-        </div>
-        }
-        </div>
-)
+    );
 }
-export default Splashscreen
-
-
-
-
-
-
-
-
-
-
-
-
+export default Landingpage;
