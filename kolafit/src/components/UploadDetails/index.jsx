87f80bd635e4a0e6771e0c0c9760a9b757798166
mercy-ndungu <mercyndungukirigo@gmail.com>
2022-11-03 13:05:04 +0300

    Modified upload Details

--- kolafit/src/components/UploadDetails/index.jsx
+++ kolafit/src/components/UploadDetails/index.jsx
@@ -1,4 +1,3 @@
-
 import React, {useState,Component} from 'react';
 // import ReactDOM from 'react-dom';
 import "./style.css"
@@ -8,36 +7,49 @@ import "../Kola.png"
 //CLICK RFC TO GENERATE LAYOUT.
 import kolafit from '../image-removebg-preview 1.png';
 import axios from "axios"
-function Details(){ 
+function Details(){
     const [rent_amount, setRent] = useState("");
-    const [rent_receipts, setFile2] = useState(null);
-    const [electricity_receipts, setElectricity] = useState(null) 
-    const [water_receipts, setFile] = useState(null)
+    const [rent_receipts, setFile2] = useState('');
+    const [electricity_receipts, setElectricity] = useState('')
+    const [water_receipts, setFile] = useState('')
     const [loan_amount, setAmount] = useState("")
-    const detailSubmit = (e) => {
-        e.preventDefault()
-        const user= { rent_amount,  rent_receipts,electricity_receipts,water_receipts,loan_amount}
-        console.log(user);
-        if (rent_amount && rent_receipts && electricity_receipts && water_receipts && loan_amount) {
-          axios.post("https://fierce-hollows-95496.herokuapp.com/api/details/",user)
-            .then(res => {
-              console.log(res)
-            })
-            .catch(error => {
-              console.log(error)
-            })
-        }
-        else {
-          console.log(user)
-          alert("invalid input")
-        }
-      }
+    // const detailSubmit = (e) => {
+    //     e.preventDefault()
+    //     const user= { rent_amount,  rent_receipts,electricity_receipts,water_receipts,loan_amount}
+    //     console.log(user);
+    //     if (rent_amount && rent_receipts && electricity_receipts && water_receipts && loan_amount) {
+    //       axios.post("https://fierce-hollows-95496.herokuapp.com/api/details/",user)
+    //         .then(res => {
+    //           console.log(res)
+    //         })
+    //         .catch(error => {
+    //           console.log(error)
+    //         })
+    //     }
+    //     else {
+    //       console.log(user)
+    //       alert("invalid input")
+    //     }
+    //   }
+    const detailSubmit = (e)=>{
+        e.preventDefault();
+        console.log("receits",rent_receipts.replace('C:\\fakepath\\',''));
+        let form_data = new FormData();
+        form_data.append('rent_amount', rent_amount);
+        form_data.append('rent_receipts',  rent_receipts.replace('C:\\fakepath\\',''));
+        form_data.append('electricity_receipts',electricity_receipts.replace('C:\\fakepath\\',''));
+        form_data.append('water_receipts', water_receipts.replace('C:\\fakepath\\',''));
+        form_data.append('loan_amount', loan_amount);
+        axios.post("https://fierce-hollows-95496.herokuapp.com/api/details/", form_data, {
+        }).then(res => {
+          console.log(res.data);
+        }).catch(err => console.log(err))
+    };
     return(
         <div className='Details'>
             <div className='logo'>
             <img  className="logo" src= {kolafit} alt='Logo'></img>
             </div>
-
         <div className='Front'>
         <form onSubmit={detailSubmit}>
             <div className="rentAmount">
@@ -60,11 +72,13 @@ function Details(){
                             ></input></div>                  */}
                 <div className="RR">
                   <p> Upload Rent Reciepts</p>
-                 <input className="reciepts"type="file"
+                 <input className="reciepts"
+                 type="file"
                     placeholder="Upload rent Reciepts" value={rent_receipts}
-                    accept="image/png, image/jpeg" 
+                    accept="image/png, image/jpeg"
                     multiple
                    onChange={(e) => {
+                    // .replace('C:\\fakepath\\','')
                     setFile2 (e.target.value);
                 }
                 }
@@ -72,7 +86,7 @@ function Details(){
                 <div className="billName">
                   <p> Upload Three Electricity Reciepts</p>
                  <input className="name"type="file"
-                    placeholder="Upload Electricity Reciepts" value={electricity_receipts} accept="image/png, image/jpeg" 
+                    placeholder="Upload Electricity Reciepts" value={electricity_receipts} accept="image/png, image/jpeg"
                    onChange={(e) => {
                     setElectricity (e.target.value);
                 }
@@ -80,7 +94,7 @@ function Details(){
                          ></input></div>
                 <div className="bill">
                   <p> Upload Three Water Bill Reciepts</p>
-                 <input className="billReceipts"type="file" accept="image/png, image/jpeg" 
+                 <input className="billReceipts"type="file"  accept="image/png, image/jpeg"
                     placeholder="Upload Water Bill Reciepts" value={water_receipts}
                    onChange={(e) => {
                 setFile (e.target.value);
@@ -97,9 +111,9 @@ function Details(){
                          ></input></div>
        <div className='bottom'>
        <Link path to='/details' ><button className='prev' type='submit' style={{width: "150px", marginBottom: "4px"}} >Back</button></Link>
-        {/* <Link path to='/final'> */}
+        <Link path to='/final'>
              <button className='nex'style={{width: "150px", marginBottom: "4px"}}>Submit</button>
-        {/* </Link> */}
+        </Link>
        </div>
         </form>
         </div>
